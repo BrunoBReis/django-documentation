6dda4132dd57a4a336af9dc324238f7cff9e2839
BrunoBReis <brunobragancadosreis@gmail.com>
2024-09-03 17:02:24 -0300

    refactor: updating vote view

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -54,14 +54,11 @@ def vote(request, question_id):
         selected_choice = question.choice_set.get(pk=request.POST["choice"])
     except (KeyError, Choice.DoesNotExist):
         # Redisplay the question voting form.
-        return render(
-            request,
-            "polls/detail.html",
-            {
-                "question": question,
-                "error_message": "You didn't select a choice.",
-            },
-        )
+        context = {
+            "question": question,
+            "error_message": "You didn't select a choice.",
+        }
+        return render(request, "polls/detail.html", context)
     else:
         selected_choice.votes = F("votes") + 1
         selected_choice.save()
